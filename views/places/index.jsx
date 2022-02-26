0e30ef422ffb34aad405bf1e9be941fd8aa992f7
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-02-26 07:52:24 -0700

    Adjust to singular for key used since multiple are rendered

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,7 +4,7 @@ const Def = require('../default')
 function index (data) {
   let placesFormatted = data.places.map((place) => {
     return (
-      <div key={'places'}>
+      <div key={'place'}>
         <h2>{place.name}</h2>
         <img src={place.pic} alt={place.name}/>
       </div>
