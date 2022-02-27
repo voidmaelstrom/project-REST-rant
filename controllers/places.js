a2711ac539e47c2dd4c9b476bb33568a156e8dd2
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-02-27 12:36:33 -0700

    Stub remaining routes from prior and apply Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -28,4 +28,24 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.post('/', (req, res) => {
+  res.send('POST /places stub')
+})
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+  res.send('GET /places/:id/rant/:rantId stub')
+})
+
 module.exports = router
