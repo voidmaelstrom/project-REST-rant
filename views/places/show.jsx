51d15a86ae257c19a1f2bc4bb03653ec5d0925cf
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-03-07 19:35:24 -0700

    Start setup of helper methods

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,6 +9,9 @@ function show (data) {
             <h2>Rating</h2>
             <h4>Not Rated</h4>
             <h2>Description</h2>
+            <img src={data.place.pic} alt={data.place.name} className="center" />
+            <h4>{data.place.showEstablished()}</h4>
+            <h4>Serving {data.place.cuisines}</h4>
             <h4>Located in {data.place.city}, {data.place.state}</h4>
           </main>
           <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
